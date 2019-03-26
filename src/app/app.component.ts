9e81a6655d9d89482b4d7f500b1dd37f06110033
Bryan von Frajer <bryanvonfrajer@Bryans-MacBook-Pro.local>
2019-03-26 10:54:01 +0100

    interval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
 
+
+import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
-  title = 'My First Angular App!';
 
+export class AppComponent implements OnInit {
   pictures = [
     { id: 1, name: 'https://images.unsplash.com/photo-1553455860-2fa544e14141?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=802&q=80' },
     { id: 2, name: 'https://images.unsplash.com/photo-1553450646-bd2a35fa7c9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80' },
@@ -17,9 +17,14 @@ export class AppComponent {
 
   imgCounter = 0;
   image = this.pictures[this.imgCounter];
+    
+  ngOnInit() {
+    setInterval(() => {
+      this.next();
+    }, 2000);
+  }
 
   prev() {
-
     if ((this.imgCounter == 0)) {
       this.imgCounter = this.pictures.length - 1;
       this.image = this.pictures[this.imgCounter];
@@ -27,27 +32,24 @@ export class AppComponent {
     } else {
       this.imgCounter--;
       this.image = this.pictures[this.imgCounter];
-
     }
-
   }
 
   next() {
     if (this.imgCounter == (this.pictures.length - 1)) {
       this.imgCounter = 0;
       this.image = this.pictures[this.imgCounter];
-
     } else {
       this.imgCounter++;
       this.image = this.pictures[this.imgCounter];
-
     }
-
   }
 
   selectImage(index) {
     this.image = this.pictures[index];
   }
+  
+  
 
 }
 
