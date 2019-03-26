dac181e456ce9795c12370ecd0c37d47fb0f4fcf
Bryan von Frajer <bryanvonfrajer@Bryans-MacBook-Pro.local>
2019-03-26 13:09:00 +0100

    clean up scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { delay } from 'q';
-import { start } from 'repl';
-import { interval } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
